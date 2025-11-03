613dbec7d5a63c2256cab428a3779721b256188a
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2025-11-03 13:22:33 +0100

    Er worden verschillende berichten laten zien op basis van gezichtsuitdrukking

--- Applicatie/camera_test.py
+++ Applicatie/camera_test.py
@@ -50,9 +50,28 @@ while True:
         if gender and age and emotion:
             cv2.putText(frame, f"{gender}, {age} jaar, {emotion}", (x, y - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-            
-            cv2.putText(frame, str(result[0]['emotion']), (x, y + h + 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 255), 1)
+
+            # cv2.putText(frame, str(result[0]['emotion']), (x, y + h + 20),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 255), 1)
+
+            if emotion == "happy":
+                reactie = "Wat ben je vandaag vrolijk!"
+            elif emotion == "sad":
+                reactie = "Het is bijna weekend, nog even doorzetten"
+            elif emotion == "angry":
+                reactie = "Ik begrijp het, het is maandagochtend"
+            elif emotion == "surprise":
+                reactie = "Is het eerder weekend?"
+            elif emotion == "fear":
+                reactie = "Is het later weekend?"
+            elif emotion == "disgust":
+                reactie = "Vakantie is zeker voorbij?"
+            else:
+                reactie = "Kin gebeure"
+
+            cv2.putText(frame, reactie, (x, y + h + 40),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
+
 
     last_results = current_results
 
